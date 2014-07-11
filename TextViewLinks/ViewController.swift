f5e0f202ffdfa69b89bd64f65031ef38206dc97f
Gene De Lisa <gene@rockhoppertech.com>
2014-07-11 11:30:47 -0400

    Initial Commit

---
+++ TextViewLinks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextViewLinks
+//
+//  Created by Gene De Lisa on 7/11/14.
+//  Copyright (c) 2014 Gene De Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
